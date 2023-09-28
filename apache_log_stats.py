4d5061664f16409a90d0162729ec3ba86d13b21f
Greg Drake <greg@madmallards.com>
2023-09-28 12:38:00 -0400

    Create apache_log_stats.py

---
+++ apache_log_stats.py
@@ -0,0 +1,82 @@
+import os
+import glob
+import re
+import datetime
+from collections import Counter
+import matplotlib.pyplot as plt
+from jinja2 import Environment, FileSystemLoader
+
+# Define the log directory and output directory
+log_dir = "/var/log/apache2/"
+output_dir = "/tmp/stats/"
+
+# Ensure the output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+# Define a function to parse Apache log files
+def parse_apache_logs(log_file):
+    date_access_counts = Counter()
+    popular_pages = Counter()
+    error_counts = Counter()
+
+    date_format = re.compile(r'\[(.*?)\]')
+    page_pattern = re.compile(r'"GET (.*?) HTTP')
+
+    with open(log_file, 'r') as log:
+        for line in log:
+            date_match = date_format.search(line)
+            page_match = page_pattern.search(line)
+
+            if date_match:
+                date_str = date_match.group(1).split()[0]
+                date_obj = datetime.datetime.strptime(date_str, '%d/%b/%Y:%H:%M:%S')
+                date_access_counts[date_obj.date()] += 1
+
+            if page_match:
+                page = page_match.group(1)
+                popular_pages[page] += 1
+
+            if "error" in log_file:
+                error_message = line.split('] ')[-1].strip()
+                error_counts[error_message] += 1
+
+    return date_access_counts, popular_pages, error_counts
+
+# Process all log files in the log directory
+log_files = glob.glob(os.path.join(log_dir, '*.log*'))
+log_files.extend(glob.glob(os.path.join(log_dir, '*.log.*')))
+log_files.extend(glob.glob(os.path.join(log_dir, 'other_vhosts_access.log*')))
+log_files.extend(glob.glob(os.path.join(log_dir, 'other_vhosts_access.log.*')))
+
+# Dictionary to store statistics for each domain
+domain_stats = {}
+
+for log_file in log_files:
+    domain = os.path.basename(log_file).split('_')[0]
+
+    if domain not in domain_stats:
+        domain_stats[domain] = {
+            'daily_access_counts': Counter(),
+            'popular_pages': Counter(),
+            'error_counts': Counter()
+        }
+
+    date_access_counts, page_counts, error_count = parse_apache_logs(log_file)
+    domain_stats[domain]['daily_access_counts'].update(date_access_counts)
+    domain_stats[domain]['popular_pages'].update(page_counts)
+    domain_stats[domain]['error_counts'].update(error_count)
+
+# Create HTML/CSS report for each domain using Jinja2 template
+env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+template = env.get_template('report_template.html')
+
+for domain, stats in domain_stats.items():
+    output_html = os.path.join(output_dir, f'{domain}_stats.html')
+    with open(output_html, 'w') as html_file:
+        html_file.write(template.render(
+            daily_access=stats['daily_access_counts'],
+            popular_pages=stats['popular_pages'].most_common(10),
+            top_errors=stats['error_counts'].most_common(10)
+        ))
+
+    print(f"Statistics for {domain} generated and saved in {output_html}")
