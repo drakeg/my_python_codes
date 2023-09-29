a117e67307cd78e1dff31e010553c3484703008d
Greg Drake <greg@madmallards.com>
2023-09-29 11:16:14 -0400

    Rename jinja file.

--- apache_log_stats.py
+++ apache_log_stats.py
@@ -90,7 +90,7 @@ def generate_domain_report(domain, log_files, output_dir):
 
     # Create HTML/CSS report for the domain
     env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
-    template = env.get_template('report_template.html')
+    template = env.get_template('report_template.jinja')
     output_html = os.path.join(output_dir, f'{domain}.html')
     with open(output_html, 'w') as html_file:
         html_file.write(template.render(
